30f9ec36c9800236050f0a06ac09ea0ee190b3aa
sumaaanth <sumanththota7@gmail.com>
2025-05-11 19:56:59 -0400

    Created Project Structure with Route, Config and Utils

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -6,10 +6,9 @@ import (
 	"net/http"
 )
 
-//it helps to parse the bodyin the create book function, json => unmarshall
+//it helps to parse the body in the create book function, json => unmarshall
 //-> reads the json body
 //-> Unmarshall(converts it into the provided interface structure)
-//-> 
 func ParseBody(r *http.Request, x interface{}){
 	if body, err := io.ReadAll(r.Body); err == nil{
 		if err := json.Unmarshal([]byte(body), x); err != nil{
